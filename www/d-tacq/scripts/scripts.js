1dddd843d8f8b3db0492e9048fd82c1c1493f575
sambelltacq <sam.bell@d-tacq.co.uk>
2024-08-09 13:52:30 +0100

    Updated banner

--- www/d-tacq/scripts/scripts.js
+++ www/d-tacq/scripts/scripts.js
@@ -9,24 +9,19 @@ function add_ssl_banner(parent){
     }
     const ssl_available = getCookie('ssl_available');    ;
     const protocol = window.location.protocol.replace(':', '');
-  
-    console.log(ssl_available)
-    protocol = 'http';
-    ssl_available = 'true';
     
     if (ssl_available === "true" && protocol == "http"){
       console.log('Inserting ssl banner')
       const https_link = window.location.href.replace('http:', 'https:');
       const ssl_banner = `
-        <div class="acqInfo" style="background-color: lightgray; padding: 10px; border: 1px solid black;margin: 5px;">
+        <div class="sslBanner">
             <h2>SSL is available</h2>
             <p>
-              Install <a href="LINK_PLACEHOLDER">Certificate</a> and go to <a href="${https_link}">https</a>
+              Install <a href="CA_LINK_PLACEHOLDER">Certificate</a> and go to <a href="${https_link}">https</a>
             </p>
         </div>
       `;
       const container = document.getElementById(parent);
-      console.log(container)
       container.insertAdjacentHTML('beforeend', ssl_banner); 
     }
 };
